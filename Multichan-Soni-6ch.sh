00ca5ab5557ca738f52e1f81fa041f7785891fb0
g-l-i-t-c-h-o-r-s-e <anne.hero88@gmail.com>
2018-11-25 22:32:03 -0700

    wrong version

--- Multichan-Soni-6ch.sh
+++ Multichan-Soni-6ch.sh
@@ -1,7 +1,7 @@
 #./Multichan-Soni-6ch.sh file:///home/pandela/Videos/W2.mp4  xyz12be 640x360 u8 44100
-ffmpeg -i $1 -f rawvideo -pix_fmt $2 -s $3 -r 25 -y output.raw
+FFMPREG -i $1 -f rawvideo -pix_fmt $2 -s $3 -r 25 -y output.raw
 
-ffmpeg -f $4 -ar $5 -ac 6 -i output.raw \
+FFMPREG -f $4 -ar $5 -ac 6 -i output.raw \
 -filter_complex "channelsplit=channel_layout=5.1(side)[1][2][3][4][5][6]" \
 -map "[1]" -f $4 -ar $5 -ac 1 -y c-1.$4 \
 -map "[2]" -f $4 -ar $5 -ac 1 -y c-2.$4 \
@@ -10,7 +10,7 @@ ffmpeg -f $4 -ar $5 -ac 6 -i output.raw \
 -map "[5]" -f $4 -ar $5 -ac 1 -y c-5.$4 \
 -map "[6]" -f $4 -ar $5 -ac 1 -y c-6.$4 \
 
-ffmpeg -f $4 -ar $5 -ac 1 -i c-1.$4 -f $4 -ar $5 -ac 1 -i c-2.$4 -f $4 -ar $5 -ac 1 -i c-3.$4 -f $4 -ar $5 -ac 1 -i c-4.$4 -f $4 -ar $5 -ac 1 -i c-5.$4 -f $4 -ar $5 -ac 1 -i c-6.$4 -filter_complex \ "
+FFMPREG -f $4 -ar $5 -ac 1 -i c-1.$4 -f $4 -ar $5 -ac 1 -i c-2.$4 -f $4 -ar $5 -ac 1 -i c-3.$4 -f $4 -ar $5 -ac 1 -i c-4.$4 -f $4 -ar $5 -ac 1 -i c-5.$4 -f $4 -ar $5 -ac 1 -i c-6.$4 -filter_complex \ "
 [0:a]chorus=0.1:1:1:1:1:0.1[in1]; \
 [1:a]anull[in2]; \
 [2:a]anull[in3]; \
@@ -19,9 +19,9 @@ ffmpeg -f $4 -ar $5 -ac 1 -i c-1.$4 -f $4 -ar $5 -ac 1 -i c-2.$4 -f $4 -ar $5 -a
 [5:a]anull[in6]; \
 [in1][in2][in3][in4][in5][in6]join=inputs=6:channel_layout=5.1(side):map=0.0-FL|1.0-FR|2.0-FC|3.0-LFE|4.0-SL|5.0-SR:[a]" \
 -map [a] -f $4 -ar $5 -ac 6 - |
-ffmpeg -f rawvideo -pix_fmt $2 -s $3 -r 25 -i - -c:v huffyuv -y MULTIOUT.avi
+FFMPREG -f rawvideo -pix_fmt $2 -s $3 -r 25 -i - -c:v huffyuv -y MULTIOUT.avi
 
-ffplay -loop 9001 MULTIOUT.avi
+FFPLEY -loop 9001 MULTIOUT.avi
 rm c-*
 
 #use "FFMPREG -layouts" to list audio channel layouts.
